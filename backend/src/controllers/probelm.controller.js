27c3047314667f1ad3d18eda8e7f413d5f2ff58b
uuuuday <krishnauday200301@gmail.com>
2025-09-28 11:27:06 +0530

    push the create probelm code

--- backend/src/controllers/probelm.controller.js
+++ backend/src/controllers/probelm.controller.js
@@ -1,4 +1,5 @@
-import { getJudge0LanguageId, submitBatch } from "../libs/judge0.lib.js"
+import { db } from "../libs/db.js"
+import { getJudge0LanguageId, pollBatchResults, submitBatch } from "../libs/judge0.lib.js"
 
 
 
@@ -8,30 +9,67 @@ export const createProblem= async(req,res)=>{
     const {title,description,difficulty,tags,examples,constraints,hints,testcases,codeSnippets, referenceSolutions 
 } = req.body
 
-    for (const [language,solutionCode] of referenceSolutions) {
-        //create a mini which feteches the languageId
-
-        const languageId = getJudge0LanguageId(language)
-
-        if(!languageId){
-            return res.status(200).json({
-                error:`language ${language} not supported`
-            })
-        }
-    }
-
-    const submissions = testcases.map(({input,output})=>({
-        source_code:solutionCode,
-        language_id:languageId,
-        stdin:input,
-        expected_output:output
-    }))
-
-    const submissionResult = await submitBatch(submissions);
-
-    const token = submissionResult.map((res)=>res.token)
-
+    try {
+        for (const [language,solutionCode] of referenceSolutions) {
+            //create a mini which feteches the languageId
     
-
-
+            const languageId = getJudge0LanguageId(language)
+    
+            if(!languageId){
+                return res.status(200).json({
+                    error:`language ${language} not supported`
+                })
+            }
+        }
+    
+        const submissions = testcases.map(({input,output})=>({
+            source_code:solutionCode,
+            language_id:languageId,
+            stdin:input,
+            expected_output:output
+        }))
+    
+        const submissionResult = await submitBatch(submissions);
+    
+        const tokens = submissionResult.map((res)=>res.token)
+    
+    
+        const results =await pollBatchResults(tokens);
+    
+        for (let i = 0; i < results.length; i++) {
+            const result = results[i];
+            console.log("Result-----", result);
+            // console.log(
+            //   `Testcase ${i + 1} and Language ${language} ----- result ${JSON.stringify(result.status.description)}`
+            // );
+            if (result.status.id !== 3) {
+              return res.status(400).json({
+                error: `Testcase ${i + 1} failed for language ${language}`,
+              });
+            }
+          }
+    
+    
+          const newProblem = await db.problem.create({
+            data:{
+                    title,description,difficulty,tags,examples,constraints,hints,testcases,codeSnippets, referenceSolutions,userId:req.user.id
+    
+            }
+          })
+    
+          return res.status(201).json({
+            success:true,
+            message:"problem created successfully",
+            problem:newProblem
+          })
+        }
+    catch (error) {
+     console.log(error);
+        return res.status(500).json({
+            error:"error while creating the problem"
+        })
+    }
 }
+
+
+
