3a855ab43d1a3eed1343a1e47f09665dfac60819
uuuuday <krishnauday200301@gmail.com>
2025-10-03 16:12:38 +0530

    add the component realated to signup code

--- frontend/src/page/SignUpform.jsx
+++ frontend/src/page/SignUpform.jsx
@@ -1,6 +1,16 @@
 import React, { useState } from 'react'
 import { z } from "zod";
 import { useForm } from "react-hook-form"
+import {zodResolver} from "@hookform/resolvers/zod"
+import { Link } from 'react-router-dom'
+import {
+  Code,
+  Eye,
+  EyeOff,
+  Loader2,
+  Lock,
+  Mail,
+} from "lucide-react";
 
 //create a scheam for the frontend validations
 
