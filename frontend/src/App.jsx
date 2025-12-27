0c4777a4e5c3e791e25450484fde1a6c1f8ccff9
uuuuday <krishnauday200301@gmail.com>
2025-12-27 09:43:38 +0530

    add the latest changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import { useAuthStore } from './store/useAuthStore'
 import { useEffect } from 'react'
 import { Loader } from "lucide-react";
 import Layout from './layout/Layout'
+import AdminRoute from './components/AdminRoute'
+import AddProblem from './page/AddProblem'
 
 
 
@@ -49,8 +51,17 @@ const App = () => {
 
     <Route 
     path="/signup"
-    element={<SignUpform/>}
+    element={!authUser ? <SignUpform/> : <Navigate to ={"/"}/>}
     />
+
+
+    <Route 
+    element={<AdminRoute/>}>
+      <Route
+      path="/add-problem"
+      element={authUser ? <AddProblem/> : <Navigate to ="/"/>}
+      />
+          </Route>
       </Routes>
     </div>
   )
---
+++ frontend/src/components/CreateProblemForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateProblemForm() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CreateProblemForm
