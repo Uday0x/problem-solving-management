08f907fd938d1ddaac9ee758cb6cd2ce7b522c0c
uuuuday <krishnauday200301@gmail.com>
2026-01-02 23:23:30 +0530

    add a change in the route

--- backend/src/routes/problemRoutes.js
+++ backend/src/routes/problemRoutes.js
@@ -10,7 +10,7 @@ problemRoutes.post("/create-problem",authMiddleware,checkAdmin,createProblem)
 problemRoutes.get("/get-all-problems",authMiddleware,getAllProblems)
 problemRoutes.get("/get-problem/:id",authMiddleware,getProblemById)
 problemRoutes.post("/update-problem/:id",authMiddleware,updateProblem)
-problemRoutes.post("/delete-problem/:id",authMiddleware,deleteProblem)
-problemRoutes.get("/get-solved-problems",authMiddleware,getAllProblemsSolvedByUser)
+problemRoutes.delete("/delete-problem/:id",authMiddleware,deleteProblem)
+problemRoutes.get("/get-solved-problem",authMiddleware,getAllProblemsSolvedByUser)
 
 export default problemRoutes;
