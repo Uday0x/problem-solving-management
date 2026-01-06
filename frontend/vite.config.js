c60dae8174488ee96c634f6b018ed6a19de08990
uuuuday <krishnauday200301@gmail.com>
2026-01-06 16:51:11 +0530

    add changes to vite and axios for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://problem-solving-management-production.up.railway.app",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
+});
