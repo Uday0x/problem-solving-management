8f6868fe4cf4df2651a7e05cce2cd602d880fc31
uuuuday <krishnauday200301@gmail.com>
2026-01-06 17:00:33 +0530

    add some changes for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,8 +2,10 @@
 import axios from "axios"
 
 
-export const axiosInstance = axios.create({    
-     baseURL: "/api/v1",
-     withCredentials: true,
+export const axiosInstance = axios.create({
+
+    
+    baseURL:import.meta.env.MODE === "development" ? "http://localhost:7000/api/v1" : import.meta.env.VITE_API_URL, //its better to verify port from the frontend //here we have 7000
+    withCredentials: true, //this is for the purpose of cookies
 })
 console.log("API URL:", import.meta.env.VITE_API_URL);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://problem-solving-management-production.up.railway.app",
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
-});
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
