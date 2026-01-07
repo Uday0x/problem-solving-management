d857efc9420e3e19588bc1740217cde1d208d958
uuuuday <krishnauday200301@gmail.com>
2026-01-07 18:50:43 +0530

    add a change for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(
     cors({
         origin: [
       "http://localhost:5173",
-      "https://codelabss.up.railway.app" // 👈 Vercel / Railway frontend
+      "https://codelabss.up.railway.app"        // 👈 Vercel / Railway frontend
     ],
         credentials:true
     })
